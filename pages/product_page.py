fce42189991bdb4407097b3e3ce7d2bb1c9a1efb
Maxim S <poplers24@gmail.com>
2022-06-27 21:33:13 +0300

    finalizing the product page test

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,11 +9,11 @@ class ProductPage(BasePage):
     def item_added_to_basket(self):
         product_title = self.browser.find_element(*ProductPageLocators.PRODUCT_TITLE).text
         message_product_title = self.browser.find_element(*ProductPageLocators.MESSAGE_TITLE_PRODUCT).text
-        assert product_title in message_product_title, "The product name in the cart does not match the product added"
+        assert product_title == message_product_title, "The product name in the cart does not match the product added"
 
     def compare_the_price_of_the_product_in_sum_basket(self):
         price_to_product = self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
         sum_basket = self.browser.find_element(*ProductPageLocators.MESSAGE_PRICE_PRODUCT).text
-        assert price_to_product in sum_basket, "The cart amount does not match the price of the product"
+        assert price_to_product == sum_basket, "The cart amount does not match the price of the product"
 
 
